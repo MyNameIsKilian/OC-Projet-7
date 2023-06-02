ea1adc7c19edcd21cedad5921564477fbc30f2be
Kilian <kilian.alliot@heavn.app>
2023-06-02 15:20:28 +0200

    Serverless installed

--- app.py
+++ app.py
@@ -1,37 +1,39 @@
-import streamlit as st
-import pandas as pd
+from flask import jsonify
+import json
+import werkzeug
 import numpy as np
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
 
-st.title('My bank app')
-# streamlit run app.py
+# app = Flask(__name__)
 
-DATE_COLUMN = 'date/time'
-DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
-         'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+# @app.route("/iris", methods=["GET"])
+def main():
+    # Charger le dataset Iris
+    iris = load_iris()
+    X_train, X_test, y_train, y_test = train_test_split(iris.data, iris.target, test_size=0.2, random_state=42)
+    model = LogisticRegression()
+    model.fit(X_train, y_train)
+    y_pred = model.predict(X_test)
+    accuracy = accuracy_score(y_test, y_pred)
+    model_params = model.get_params()
+    json_params = json.dumps(model_params)
+    
+    response = {
+        'dataset': 'iris',
+        'model_parameters': json_params,
+        'accuracy': accuracy
+    }
+    
+    return jsonify(response)
 
-@st.cache
-def load_data(nrows):
-    data = pd.read_csv(DATA_URL, nrows=nrows)
-    lowercase = lambda x: str(x).lower()
-    data.rename(lowercase, axis='columns', inplace=True)
-    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
-    return data
+    # pickle.save
+    # pickle.load
+    # load model from s3
 
-data_load_state = st.text('Loading data...')
-data = load_data(10000)
-data_load_state.text("Done! (using st.cache_data)")
 
-if st.checkbox('Show raw data'):
-    st.subheader('Raw data')
-    st.write(data)
 
-st.subheader('Number of pickups by hour')
-hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
-st.bar_chart(hist_values)
 
-# Some number in the range 0-23
-hour_to_filter = st.slider('hour', 0, 23, 17)
-filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
 
-st.subheader('Map of all pickups at %s:00' % hour_to_filter)
-st.map(filtered_data)
