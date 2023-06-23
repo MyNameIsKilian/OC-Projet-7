da72b5ec126ae9cbc5cfa1649a537738637e080a
Kilian <kilian.alliot@heavn.app>
2023-06-23 11:02:06 +0200

    Fix test v2

--- tests/functions.py
+++ tests/functions.py
@@ -1,10 +1,11 @@
 import pickle
 
-loaded_model = pickle.load(open('lgbm_model.sav', 'rb'))
+
 # loaded_pipeline = pickle.load(open('pipeline.sav', 'rb'))
 #
 def add_numbers(a,b):
     return a+b
 
 def display_model_leaves():
+    loaded_model = pickle.load(open('lgbm_model.sav', 'rb'))
     return loaded_model.num_leaves
