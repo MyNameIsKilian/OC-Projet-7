1bf6ee3cdba3567dd2e40a80bd643330d66984b0
Kilian <kilian.alliot@heavn.app>
2023-07-07 11:04:54 +0200

    Add test with loaded shap explainer

--- tests/functions.py
+++ tests/functions.py
@@ -12,3 +12,7 @@ def predict_class():
     sample_data = pd.read_csv('X_train_sample.csv', index_col=[0])
     loaded_pipeline = pickle.load(open('pipeline.sav', 'rb'))
     return loaded_pipeline.predict(sample_data.iloc[1:2,:])
+
+def display_explainer_expected_values():
+    loaded_explainer = pickle.load(open('shap_explainer.sav', 'rb'))
+    return loaded_explainer.expected_value
--- tests/test_class.py
+++ tests/test_class.py
@@ -1,4 +1,5 @@
 from functions import *
+from IPython.display import display
 
 class TestClass:
     def test_add_numbers(self):
@@ -9,3 +10,6 @@ class TestClass:
 
     def test_predict_class(self):
         assert predict_class() == [0]
+
+    def test_expected_value_from_explainer(self):
+        assert display_explainer_expected_values() == [0.36253430662611813, -0.36253430662611813]
