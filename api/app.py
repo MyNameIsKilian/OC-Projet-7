601470043cc3c468ce3bdb7b2363e9ccdb9da691
Kilian <kilian.alliot@heavn.app>
2023-07-28 13:31:40 +0200

    Add webhook function

--- api/app.py
+++ api/app.py
@@ -5,6 +5,7 @@ import pickle
 import numpy as np
 # import json
 from sklearn.neighbors import NearestNeighbors
+import git
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
@@ -28,6 +29,16 @@ def main():
         'api':'test push from local',
         # 'data_shape': df_final.shape
     }
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    if request.method == 'POST':
+        repo = git.Repo('path/to/git_repo')
+        origin = repo.remotes.origin
+        origin.pull()
+        return 'Updated PythonAnywhere successfully', 200
+    else:
+        return 'Wrong event type', 400
 
 @app.route('/prediction', methods=['POST'])
 def lgbm_prediction():
